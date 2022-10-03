5c9d881d96ba85d8e386c480b2f2ba14dc4f0abc
anacarolcortez <anacarolcortez@hotmail.com>
2022-10-03 16:41:53 -0300

    correção de retornos nulos em endereços

--- src/models/address.py
+++ src/models/address.py
@@ -6,7 +6,7 @@ import json
 async def create_address(address_collection, address):
     try:
         already_exists = await get_address_by_zipcode(address_collection, address.zipcode)
-        if already_exists:
+        if type(already_exists) == dict:
             raise Exception("Um endereço com este cep já está cadastrado no sistema")
         else:
             data = await address_collection.insert_one(jsonable_encoder(address))
@@ -19,8 +19,10 @@ async def create_address(address_collection, address):
 async def get_address_by_id(address_collection, address_id):
     try:
         address = await address_collection.find_one({'_id': ObjectId(address_id)})
-        if address:
+        if type(address) == dict:
             return json.loads(json_util.dumps(address))
+        else:
+            raise Exception("Endereço não encontrado")
     except Exception as e:
         return f'get_address_by_id.error: {e}'     
     
@@ -28,8 +30,10 @@ async def get_address_by_id(address_collection, address_id):
 async def get_address_by_zipcode(address_collection, zipcode):
     try:
         data = await address_collection.find_one({'zipcode': zipcode})
-        if data:
+        if type(data) == dict:
             return json.loads(json_util.dumps(data))
+        else:
+            raise Exception("Endereço não encontrado")
     except Exception as e:
         return f'get_address_by_zipcode.error: {e}'   
 
