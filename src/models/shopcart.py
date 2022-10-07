8587e3b3d36a27b024081e059d74bd8cd2893908
lais-ches <lais.ches@gmail.com>
2022-10-06 21:25:05 -0300

    novas alterações update cart

--- src/models/shopcart.py
+++ src/models/shopcart.py
@@ -1,5 +1,6 @@
 from ast import IsNot
 from itertools import product
+import logging
 from multiprocessing.connection import wait
 from urllib import response
 from xmlrpc import client
@@ -7,7 +8,7 @@ from src.models.clients import get_client_by_email
 from src.models.product import get_product_by_code
 from src.schemas.shopcart import ShopcartSchema, UpdateShopcartSchema
 from fastapi.encoders import jsonable_encoder
-from src.services.shopcart import find_opened_cart, find_product_in_cart, insert_cart, update_opened_cart
+from src.services.shopcart import find_opened_cart, find_product_in_cart, insert_cart, update_opened_cart, update_opened_cart_insert_new_product
 
 
 async def create_shopcart(shopcarts_collection, clients_collection, products_collection, email, code, new_cart):
@@ -45,14 +46,18 @@ async def update_cart(shopcarts_collection, email, product, cart, code, new_cart
     # criar função:validar pelo codigo do produto se ele já existe no carrinho
     has_product = await find_product_in_cart(shopcarts_collection, email, code)
     if has_product:
-        pass #criar um objeto que passa a quantidade e valor  
+        shopcart_data = UpdateShopcartSchema(
+            quantity_cart=quantity_cart(cart.quantity_cart, new_cart.quantity_cart),
+            value=value_cart(new_cart.quantity_cart, product.price, cart.value)
+        )
+        response = await update_opened_cart(shopcarts_collection, email, shopcart_data)
     else:
         shopcart_data = UpdateShopcartSchema(
             products=product,
             quantity_cart=quantity_cart(cart.quantity_cart, new_cart.quantity_cart),
             value=value_cart(new_cart.quantity_cart, product.price, cart.value)
         )
-    response = await update_opened_cart(shopcarts_collection, email, shopcart_data)
+        response = await update_opened_cart_insert_new_product(shopcarts_collection, email, shopcart_data)
     if response is not None:
         return response
     raise Exception("Erro ao atualizar carrinho de compras")
