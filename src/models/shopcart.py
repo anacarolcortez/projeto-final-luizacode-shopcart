93595d332aa4fb6dc2984602910304caef9ea14f
anacarolcortez <anacarolcortez@hotmail.com>
2022-10-07 11:32:17 -0300

    ajuste tipagem

--- src/models/shopcart.py
+++ src/models/shopcart.py
@@ -1,9 +1,3 @@
-from ast import IsNot
-from itertools import product
-import logging
-from multiprocessing.connection import wait
-from urllib import response
-from xmlrpc import client
 from src.models.clients import get_client_by_email
 from src.models.product import get_product_by_code
 from src.schemas.shopcart import ShopcartSchema, UpdateShopcartSchema
@@ -12,7 +6,6 @@ from src.services.shopcart import find_opened_cart, find_product_in_cart, insert
 
 
 async def create_shopcart(shopcarts_collection, clients_collection, products_collection, email, code, new_cart):
-    # receber a quantidade de produto a adicionar no carrinho
     try:
         client_data = await get_client_by_email(clients_collection, email)
 
@@ -29,7 +22,7 @@ async def create_shopcart(shopcarts_collection, clients_collection, products_col
                 client=client_data,
                 products=[product],
                 is_open=True,
-                quantity_cart=new_cart.quantity_cart,
+                quantity_cart= new_cart.quantity_cart,
                 value=new_cart.quantity_cart * product["price"]
             )
             response = await insert_cart(shopcarts_collection, jsonable_encoder(shopcart_data))
@@ -43,19 +36,18 @@ async def create_shopcart(shopcarts_collection, clients_collection, products_col
 
 
 async def update_cart(shopcarts_collection, email, product, cart, code, new_cart):
-    # criar função:validar pelo codigo do produto se ele já existe no carrinho
     has_product = await find_product_in_cart(shopcarts_collection, email, code)
     if has_product:
         shopcart_data = UpdateShopcartSchema(
-            quantity_cart=quantity_cart(cart.quantity_cart, new_cart.quantity_cart),
-            value=value_cart(new_cart.quantity_cart, product.price, cart.value)
+            quantity_cart = get_quantity_cart(cart['quantity_cart'], new_cart.quantity_cart),
+            value = get_value_cart(new_cart.quantity_cart, product['price'], cart['value'])
         )
         response = await update_opened_cart(shopcarts_collection, email, shopcart_data)
     else:
         shopcart_data = UpdateShopcartSchema(
-            products=product,
-            quantity_cart=quantity_cart(cart.quantity_cart, new_cart.quantity_cart),
-            value=value_cart(new_cart.quantity_cart, product.price, cart.value)
+            products = product,
+            quantity_cart = get_quantity_cart(cart['quantity_cart'], new_cart.quantity_cart),
+            value = get_value_cart(new_cart.quantity_cart, product['price'], cart['value'])
         )
         response = await update_opened_cart_insert_new_product(shopcarts_collection, email, shopcart_data)
     if response is not None:
@@ -79,15 +71,10 @@ async def compare_product_quantity(cart_quantity, products_collection, product):
     return product_quantity >= cart_quantity
 
 
-async def quantity_cart(cart_quantity, product_quantity):
+async def get_quantity_cart(cart_quantity, product_quantity):
     return cart_quantity + product_quantity
 
 
-async def value_cart(product_quantity, product_value, cart_value):
+async def get_value_cart(product_quantity, product_value, cart_value):
     cart_value += product_quantity * product_value
     return cart_value
-
-    # validar quantidade do produto no estoque
-    # comparar se o estoque é maior ou igual do que a quantidade solicitada pelo carrinho
-    # modificar o estoque atual do produto
-    # multiplicar quantidade x preço para ter o valor do carrinho
