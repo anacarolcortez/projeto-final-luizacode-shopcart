b231662912ae11732e58fc5548ead17a387c34b8
anacarolcortez <anacarolcortez@hotmail.com>
2022-10-11 11:49:44 -0300

    comentando main para suporte ao heroku

--- main.py
+++ main.py
@@ -13,5 +13,5 @@ app.include_router(stocks.router)
 
 
 
-if __name__ == '__main__':
-    uvicorn.run("main:app", port=8000, reload=True, access_log=False)
+# if __name__ == '__main__':
+#     uvicorn.run("main:app", port=8000, reload=True, access_log=False)
